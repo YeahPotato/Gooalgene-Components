7faf69923e82b9238125e5cebbfa2fbf3fb8b398
yangwd <277637411@qq.com>
2018-03-05 10:11:21 +0800

    add : radio-group

--- packages/radio/src/radio-group.vue
+++ packages/radio/src/radio-group.vue
@@ -5,8 +5,33 @@
 </template>
 
 <script>
+import emitter from "../../../utils/emitter";
+
 export default {
   name: "GaRadioGroup",
+
+  mixins: [emitter],
+
+  props: {
+    value: {}
+  },
+
+  watch: {
+    value: function(newVal) {
+      this.broadcast("GaRadio", "toChild", [newVal]);
+    }
+  },
+
+  mounted() {
+    // recive radio data
+    this.$on("childActive", childVal => {
+      this.$emit("input", childVal);
+      this.$emit("change", childVal);
+    });
+
+    // broadcast self data
+    this.broadcast("GaRadio", "toChild", [this.value]);
+  }
 };
 </script>
 
