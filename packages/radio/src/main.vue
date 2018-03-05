2b8073ba879af51890227e9c0105dbf97b724112
yangwd <277637411@qq.com>
2018-03-05 10:11:11 +0800

    update : radio component

--- packages/radio/src/main.vue
+++ packages/radio/src/main.vue
@@ -3,7 +3,10 @@
 <!-- <ga-radio v-model="radioData"  label="radio">单选框1</ga-radio> -->
 <template>
     <label class="ga-radio" 
-        :class="{'is-active':isActive,'is-disabled':disabled}"
+        :class="{
+          'is-active':isActive,
+          'is-disabled':disabled
+        }"
         @click="handlerClick"
         >
         <span class="ga-radio_input">
@@ -16,13 +19,31 @@
 </template>
 
 <script>
+import emitter from "../../../utils/emitter";
+
 export default {
   name: "GaRadio",
+
+  mixins: [emitter],
+
   data() {
     return {
-      isActive: false
+      isGroup: false,
+      groupVal: null
     };
   },
+
+  computed: {
+    isActive() {
+      if (this.disabled) return;
+      if (this.isGroup) {
+        return this.label === this.groupVal;
+      } else {
+        return this.label === this.value;
+      }
+    }
+  },
+
   props: {
     label: {
       type: String
@@ -33,23 +54,30 @@ export default {
     },
     value: {}
   },
+
   watch: {
-    isActive: function(newVal, oldVal) {
-      if (newVal) {
-        this.$emit("input", this.label);
-      }
-    },
-    value: function(newVal, oldVal) {
-      this.isActive = newVal === this.label;
+    value: function(newVal) {
+      this.groupVal = newVal;
     }
   },
+
   methods: {
     handlerClick() {
-      if (!this.disabled) this.isActive = true;
+      // no group
+      if (!this.isGroup) {
+        this.$emit("input", this.label);
+      } else {
+        this.dispatch("GaRadioGroup", "childActive", [this.label]);
+      }
     }
   },
-  created() {
-    this.isActive = this.label == this.value;
+
+  mounted() {
+    // init components
+    this.$on("toChild", parentData => {
+      this.isGroup = true;
+      this.groupVal = parentData;
+    });
   }
 };
 </script>
