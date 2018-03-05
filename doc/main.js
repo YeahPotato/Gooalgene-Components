ecfe4fb515e412657d9861d97c1acb1e3f2d8a3f
yangwd <277637411@qq.com>
2018-03-05 17:00:54 +0800

    add : router

--- doc/main.js
+++ doc/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueRouter from 'vue-router';
+import routes from './router.config';
+
 import './assets/scss/index.scss';
 
 import GooalgeneUI from '../packages/index';
 // import { gaSidebar , gaIcon,gaButton,gaMore } from '../packages/index';
-console.log(GooalgeneUI)
 // console.log(gaSidebar,gaButton,gaIcon,gaMore)
 
 import '../packages/style/default.scss';
+
 // console.log(GooalgeneUI);
 // console.log(gaSidebar)
 // Vue.use(gaSidebar);
@@ -15,9 +18,16 @@ import '../packages/style/default.scss';
 // Vue.use(gaButton);
 // Vue.use(gaMore);
 // Vue.use(gaSidebar);
-Vue.use(GooalgeneUI)
+
+Vue.use(GooalgeneUI);
+Vue.use(VueRouter);
+
+// routes
+const router = new VueRouter({ linkExactActiveClass: 'active', linkActiveClass: 'active', routes });
+
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
