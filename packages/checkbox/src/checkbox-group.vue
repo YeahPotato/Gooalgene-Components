22ad084df6a880153d5af05dd3aa8564368fcee0
yangwd <277637411@qq.com>
2018-03-06 16:34:20 +0800

    add : check-group

---
+++ packages/checkbox/src/checkbox-group.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="ga-checkbox__group">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import emitter from "../../../utils/emitter";
+
+export default {
+  name: "GACheckboxGroup",
+  mixins: [emitter],
+  data() {
+    return {
+      arr: []
+    };
+  },
+  props: {
+    value: {
+      type: Array
+    }
+  },
+  methods: {
+    broad() {
+      this.arr.forEach((val, index) => {
+        this.broadcast("GACheckbox", "boradcastCheckboxItem", [val, index]);
+      });
+    }
+  },
+  mounted() {
+    this.arr = [...this.value];
+    // broadcast value
+    this.broad();
+    // recive
+    this.$on("sendToGACheckboxGroup", (val, index) => {
+      val === null ? this.arr.splice(index, 1) : this.arr.push(val);
+      this.$emit("input", this.arr);
+      this.broad();
+    });
+  }
+};
+</script>
+
